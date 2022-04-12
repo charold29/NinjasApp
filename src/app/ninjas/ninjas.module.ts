c98cca22f9ab75c3e193e83126327c50b8a981cb
Harold Rojas <harolrop14@gmail.com>
2022-04-11 19:12:59 -0500

    paginas para auth y ninjas agregadas

--- src/app/ninjas/ninjas.module.ts
+++ src/app/ninjas/ninjas.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddComponent } from './pages/add/add.component';
+import { SearchComponent } from './pages/search/search.component';
+import { ViewComponent } from './pages/view/view.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddComponent,
+    SearchComponent,
+    ViewComponent,
+    HomeComponent,
+    ListComponent
+  ],
   imports: [
     CommonModule
   ]
