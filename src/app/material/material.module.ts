7a4859e0c349d6033cb80a8457779d8725ca5b97
charold-legion-s7 <harolrop14@gmail.com>
2022-04-19 22:33:27 -0500

    matgridlistitem y card

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -13,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatGridListModule,
     MatIconModule,
     MatListModule,
     MatProgressBarModule,
