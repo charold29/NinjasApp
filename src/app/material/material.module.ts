b0c9f85cca7d1f332c0d578fc991b3ff3522a896
charold-legion-s7 <harolrop14@gmail.com>
2022-04-19 21:57:28 -0500

    angular material card

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -10,6 +11,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   declarations: [],
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
