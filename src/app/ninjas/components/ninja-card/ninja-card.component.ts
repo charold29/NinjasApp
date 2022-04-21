cd31a80c6bb6e4411ef98132013f2bb7d299d42c
charold-legion-s7 <harolrop14@gmail.com>
2022-04-21 16:11:28 -0500

    new component

---
+++ src/app/ninjas/components/ninja-card/ninja-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ninja-card',
+  templateUrl: './ninja-card.component.html',
+  styleUrls: ['./ninja-card.component.css']
+})
+export class NinjaCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
