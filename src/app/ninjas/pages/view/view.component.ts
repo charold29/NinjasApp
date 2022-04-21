adac06b28a0a1aa27b40517e00398bec8d965228
charold-legion-s7 <harolrop14@gmail.com>
2022-04-21 18:57:32 -0500

    activatedroute

--- src/app/ninjas/pages/view/view.component.ts
+++ src/app/ninjas/pages/view/view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute ) { }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params
+      .subscribe( ({ id }) => console.log(id) )
+
+    // const id = this.activatedRoute.snapshot.paramMap.get('id');
+    // console.log(id)
+
   }
 
 }
