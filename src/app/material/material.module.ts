13570860acd7eedc08fc7733aa8907d6c72b57c1
charold-legion-s7 <harolrop14@gmail.com>
2022-04-21 18:59:53 -0500

    import mat-progress-spinner

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-//import { MatGridListModule } from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -14,10 +15,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatButtonModule,
     MatCardModule,
-    //MatGridListModule,
+    MatGridListModule,
     MatIconModule,
     MatListModule,
     MatProgressBarModule,
+    MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule,
   ]
