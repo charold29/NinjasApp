4f0bcb47c9c548ef1db7510a2faa7b1a09c69836
charold-legion-s7 <harolrop14@gmail.com>
2022-04-22 04:26:39 -0500

    techniques

--- src/app/ninjas/components/array-chips/array-chips.component.ts
+++ src/app/ninjas/components/array-chips/array-chips.component.ts
@@ -1,13 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-array-chips',
   templateUrl: './array-chips.component.html',
   styleUrls: ['./array-chips.component.css']
 })
-export class ArrayChipsComponent {
+
+export class ArrayChipsComponent implements OnInit {
 
   @Input() array!: string[];
   @Input() text!: string;
+
+  isSelected: boolean = true
+
+  ngOnInit(): void {
+
+    console.log(this.array[0])
+
+    if (this.text != "Main Technique")
+      this.isSelected = false
+
+  }
+
+
 
 }
